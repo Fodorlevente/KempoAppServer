72eb5dfa7271935cdbe7fd9fe034e588d24ec636
Levente Fodor <levilevi183@gmail.com>
2020-09-27 22:22:23 +0200

    Add body-parser to app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => {
     res.send('API Test');
@@ -11,3 +16,4 @@ app.listen(port, () => {
     console.log(`Running on ${port}`);
 });
 
+module.exports = app;
