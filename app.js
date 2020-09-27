f4a6d6003796889db980cc524894b7aef01b434d
Levente Fodor <levilevi183@gmail.com>
2020-09-27 22:26:06 +0200

    Add if statement for selecting the Test and the Production environment for the database

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+if (process.env === 'Test') {
+    console.log('This is the test environment');
+} else {
+    console.log('This is the production environment');
+}
+
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
