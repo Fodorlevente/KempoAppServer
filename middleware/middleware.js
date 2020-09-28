674674e36420538074da818b74e553b08d28344a
Levente Fodor <levilevi183@gmail.com>
2020-09-28 15:45:09 +0200

    Add apicache for the middleware

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,6 +1,9 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const apicache = require('apicache');
+
+let cache = apicache.middleware;
 
 // setup global middleware here
 module.exports = (app) => {
@@ -8,4 +11,5 @@ module.exports = (app) => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(cookieParser());
+    app.use(cache('5 minutes'));
 };
