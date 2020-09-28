461d3c826c7fd32a6c79d9d76102b3e5f6a94a5d
Levente Fodor <levilevi183@gmail.com>
2020-09-28 15:40:51 +0200

    Update API routes for RESTFULL design

--- api/user/user.routes.js
+++ api/user/user.routes.js
@@ -2,7 +2,11 @@ const controller = require('./user.controller');
 const express = require('express');
 const router = express.Router();
 
-router.get('/allUsers', controller.allUsers)
-router.post('/addUser', controller.saveUser);
+router.get('/users', controller.allUsers)
+router.post('/users', controller.saveUser);
+// router.put('/:id', controller.updateUser);
+// router.delete('/:id', controller.deleteUser);
+
+
 
 module.exports = router;
