b570cff6b2f0ce3b13d07eb0586dc354e95ad8eb
Levente Fodor <levilevi183@gmail.com>
2020-10-03 20:44:41 +0200

    Configure express validator for the user routes to validate the body of the request

--- api/user/auth.controller.js
+++ api/user/auth.controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt')
 const db = require('../../models/db');
 const salt = bcrypt.genSaltSync(10);
+const { validationResult } = require('express-validator');
 
 exports.saveUser = async (req, res) => {
     try{
@@ -15,8 +16,24 @@ exports.saveUser = async (req, res) => {
    
 }
 
-exports.checkUser = (req, res) => {
-    console.log("checkUser");
+//TESTTTTTT
+exports.checkUser = async (req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+
+        res.status(403).send(errors, req, res);
+        return false;
+    }
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        res.status(200).send({
+            "pass": hashedPassword,
+            "salt": salt
+        });
+    } catch {
+        res.status(403).send();
+    }
 }
 
 
--- api/user/user.routes.js
+++ api/user/user.routes.js
@@ -1,18 +1,44 @@
 const authController = require('./auth.controller');
 
 const express = require('express');
+const { check } = require('express-validator');
 const router = express.Router();
 
-const { body, check, oneOf, validationResult } = require('express-validator');
 
-router.post('/user/registration', oneOf([
-    [
-      check('email').exists(),
-      check('password').exists()
-    ]
-  ]),
-    authController.saveUser);
-router.post('/user/login', authController.checkUser);
+const WRONG_PASSWORD_MSG = `Password should not be empty, 
+                  minimum eight characters,
+                  at least one letter, 
+                  one number and one special character`;
+
+const password_regex = new RegExp("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/", "i");
+
+router.post('/user/registration',
+  [
+    check('email', 'Email is not valid')
+      .exists()
+      .normalizeEmail()
+      .isEmail(),
+    check('password', 'Password field is required')
+      .exists()
+      .withMessage(WRONG_PASSWORD_MSG)
+      .isLength({ min: 8 })
+      .matches(password_regex)
+  ],
+  authController.saveUser);
+
+router.post('/user/login',
+  [
+    check('email', 'Email is not valid')
+      .exists()
+      .normalizeEmail()
+      .isEmail(),
+    check('password', 'Password field is required')
+      .exists()
+      .withMessage(WRONG_PASSWORD_MSG)
+      .isLength({ min: 8 })
+      .matches(password_regex)
+  ],
+  authController.checkUser);
 
 
 module.exports = router;
