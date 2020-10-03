b903f8321f88a4a93cf5dc413d694d9be133bb2d
Levente Fodor <levilevi183@gmail.com>
2020-10-03 19:25:07 +0200

    Define new sql associations

--- models/associations.js
+++ models/associations.js
@@ -1,4 +1,6 @@
 // module for all the db associations
 module.exports = (db) => {
-    db.models.Team.hasMany(db.models.User);
+    db.models.Team.hasMany(db.models.Competitor);
+    db.models.Team.belongsTo(db.models.User);
+    db.models.Credential.belongsTo(db.models.User);
 }
