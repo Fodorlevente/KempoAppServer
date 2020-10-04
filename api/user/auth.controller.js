4f396d49ea7cad43ca8335f83817ed3f4b96ffd9
Levente Fodor <levilevi183@gmail.com>
2020-10-04 23:21:38 +0200

    Add functionalities for login adn registration

--- api/user/auth.controller.js
+++ api/user/auth.controller.js
@@ -1,40 +1,58 @@
-const bcrypt = require('bcrypt')
-const db = require('../../models/db');
-const salt = bcrypt.genSaltSync(10);
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
 
-exports.saveUser = async (req, res) => {
-    try{
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        res.status(200).send({
-            "pass": hashedPassword,
-            "salt": salt
-        });
-    } catch {
-        res.status(403).send();
-    }
-   
-}
+const db = require('../../models/db');
 
-//TESTTTTTT
-exports.checkUser = async (req, res) => {
+const validateRequest = (req, res) => {
     const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
-
+    if (!errors.isEmpty()) {
         res.status(403).send(errors, req, res);
         return false;
     }
-    try{
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        res.status(200).send({
-            "pass": hashedPassword,
-            "salt": salt
-        });
+}
+
+exports.saveUser = async (req, res) => {
+    validateRequest(req, res);
+    try {
+        const user = await db.models.User.create({ "email": req.body.email });
+        const salt = await bcrypt.genSaltSync(saltRounds);
+        const hash = await bcrypt.hashSync(req.body.password, salt);
+        db.models.Credential.create(
+            {
+                "password": hash,
+                "UserId": user.id
+            }
+        )
+            .then(credential => {
+                res.status(200).send(credential);
+            })
     } catch {
         res.status(403).send();
     }
 }
 
-
-
+exports.checkUser = async (req, res) => {
+    validateRequest(req, res);
+    try {
+        const user = await db.models.User.findOne({ where: { "email": req.body.email } });
+        if ( user === null){
+            res.status(403).send();
+        } else {
+            const credentials = await db.models.Credential.findOne({
+                where : {
+                    "UserId": user.id
+                }
+            });
+            const isPasswordMatch = await bcrypt.compare(req.body.password, credentials.password);
+            if(isPasswordMatch){
+                res.status(200).send();
+            } else {
+                res.status(403).send();
+            }
+        }
+    } catch {
+        res.status(403).send();
+    }
+}
